248bb16b941562704c4fc8813d7ac28ec380e0d8
NasVa <aska.vakulchik@mail.ru>
2022-01-19 11:41:04 +0300

    modified tester dll

---
+++ TesterDll/Tester.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TesterDll
+{
+    public class Tester
+    {
+        public int field;
+        public int Property { get; set; }
+        public void TestMethod() { }
+
+        public int field2;
+    }
+}
