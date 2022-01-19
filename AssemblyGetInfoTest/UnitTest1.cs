11eb42fef0f1164e6ef54e21b53c43324289eccf
NasVa <aska.vakulchik@mail.ru>
2022-01-19 11:54:06 +0300

    added unit tests

---
+++ AssemblyGetInfoTest/UnitTest1.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+
+namespace AssemblyGetInfoTest
+{
+    public class Tests
+    {
+        private Node Setup()
+        {
+            IGetInfo getter = new AssemblyGetInfo();
+            return getter.GetInfoFromFile("TesterDll.dll");
+        }
+
+        [TestMethod]
+        public void LoadingAssemblyTest()
+        {
+            Node actual = Setup();
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void AssemblyNameTest()
+        {
+            Node actual = Setup();
+            Assert.IsTrue(actual.Content.StartsWith("TesterDll"));
+        }
+
+        [TestMethod]
+        public void TypeNameTest()
+        {
+            Node actual = Setup();
+            Assert.AreEqual("Tester", actual.Nodes[0].Nodes[0].Content);
+        }
+
+        [TestMethod]
+        public void FieldsIdentifyTest()
+        {
+            Node actual = Setup();
+            Assert.AreEqual("Fields", actual.Nodes[0].Nodes[0].Nodes[0].Content);
+        }
+
+        [TestMethod]
+        public void PropertiesIdentifyTest()
+        {
+            Node actual = Setup();
+            Assert.AreEqual("Properties", actual.Nodes[0].Nodes[0].Nodes[1].Content);
+        }
+
+        [TestMethod]
+        public void MethodsIdentifyTest()
+        {
+            Node actual = Setup();
+            Assert.AreEqual("Methods", actual.Nodes[0].Nodes[0].Nodes[2].Content);
+        }
+
+    }
+}
